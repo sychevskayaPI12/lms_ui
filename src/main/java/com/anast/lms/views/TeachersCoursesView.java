f2ecf226374aae638c2bbcea788ccfb16ebf1ff8
asychevskaya <L38F2X3nYz>
2023-12-11 01:44:50 +0300

    получение курсов преподавателя

--- src/main/java/com/anast/lms/views/TeachersCoursesView.java
+++ src/main/java/com/anast/lms/views/TeachersCoursesView.java
@@ -1,13 +1,20 @@
 package com.anast.lms.views;
 
+import com.anast.lms.model.Course;
+import com.anast.lms.model.CourseSearchType;
 import com.anast.lms.model.UserProfileInfo;
+import com.anast.lms.service.StudyUtils;
 import com.anast.lms.service.external.StudyServiceClient;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Route(value = "/my_courses/teacher")
@@ -26,8 +33,63 @@ public class TeachersCoursesView extends VerticalLayout {
     public TeachersCoursesView(StudyServiceClient studyClient, UserProfileInfo profileInfo) {
         this.studyClient = studyClient;
         this.profileInfo = profileInfo;
+
         setWidthFull();
         setHeightFull();
-        add(new Label("Тут будут курсы преподавателя"));
+        build();
+    }
+
+    private void build() {
+
+        if(profileInfo.getTeacherInfo() == null) {
+            return;
+        }
+
+        HorizontalLayout filterLayout = new HorizontalLayout();
+
+        select = new Select<>();
+        select.setItems(Arrays.stream(CourseSearchType.values())
+                .map(CourseSearchType::getDisplay).collect(Collectors.toList()));
+        select.setValue(CourseSearchType.ACTIVE.getDisplay());
+        select.addValueChangeListener(e -> selectChangeListener());
+
+        //todo filters fields + button
+        filterLayout.add(select);
+
+        coursesListLayout = new VerticalLayout();
+        coursesListLayout.setPadding(false);
+
+        //todo add scroll
+
+        fillCoursesList(true);
+
+        add(select, coursesListLayout);
+
+    }
+
+    private void selectChangeListener() {
+        Boolean searchMode = StudyUtils.defineSearchMode(select.getValue());
+        //todo get filters
+        fillCoursesList(searchMode);
+    }
+
+    private void fillCoursesList(Boolean searchMode) {
+        fillCoursesList(null, null, null, searchMode);
+    }
+
+    private void fillCoursesList(String specialty, String form, String stage, Boolean searchMode) {
+
+        coursesListLayout.removeAll();
+        List<Course> courseList = studyClient.getTeacherCourses(
+                profileInfo.getLogin(), specialty, form, stage, searchMode);
+
+        courseList.forEach(course -> {
+            coursesListLayout.add(createCourseDisplayItem(course));
+        });
+    }
+
+    private HorizontalLayout createCourseDisplayItem(Course course) {
+        //todo
+        return new HorizontalLayout();
     }
 }
