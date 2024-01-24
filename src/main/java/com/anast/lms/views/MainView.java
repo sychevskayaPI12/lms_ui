72703b4619ec6067696bc68fdc5bd1a2e9d1c2a7
asychevskaya <L38F2X3nYz>
2024-01-24 03:18:09 +0300

    плашечка модератора

--- src/main/java/com/anast/lms/views/MainView.java
+++ src/main/java/com/anast/lms/views/MainView.java
@@ -14,10 +14,12 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Route(layout=MainLayout.class)
 @PageTitle("Главная | LMS")
@@ -56,45 +58,69 @@ public class MainView extends VerticalLayout {
 
     private void addInfoLayout(UserProfile profileInfo) {
         HorizontalLayout infoLayout = new HorizontalLayout();
-        if(profileInfo.getTeacherInfo() == null && profileInfo.getStudentInfo() == null) {
-            //todo
-            infoLayout.add(new Button("Добавить информацию"));
-        } else {
 
-            if(profileInfo.getStudentInfo() != null) {
-                VerticalLayout studentHelloLayout = new VerticalLayout();
-                Label label1 = new Label(String.format("Студент %s курса",
-                        profileInfo.getStudentInfo().getCourse()));
-                label1.getStyle().set("width", "max-content");
-                Label label2 = new Label(String.format("Группа %s", profileInfo.getStudentInfo().getGroupCode()));
-                label2.getStyle().set("width", "max-content");
-                studentHelloLayout.add(label1, label2);
-                studentHelloLayout.getStyle()
-                        .set("background-color", "lavenderblush")
-                        .set("border-radius", "var(--lumo-border-radius-s)")
-                        .set("width", "max-content");
-                studentHelloLayout.setSpacing(false);
+        List<String> roles = securityService.getAuthenticatedUser().getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).collect(Collectors.toList());
 
-                infoLayout.add(studentHelloLayout);
-            }
-            if(profileInfo.getTeacherInfo() != null) {
-
-                VerticalLayout teacherHelloLayout = new VerticalLayout();
-                //todo
-                //teacherHelloLayout.add(new Label(profileInfo.getTeacherInfo().toString()));
-                profileInfo.getTeacherInfo().getPositions().forEach(pos -> {
-                    Label posLabel = new Label(pos.toString());
-                    teacherHelloLayout.add(posLabel);
-                });
-                teacherHelloLayout.getStyle()
-                        //lavenderblush
-                        .set("background-color", "lavender")
-                        .set("border-radius", "var(--lumo-border-radius-s)");
-
-                infoLayout.add(teacherHelloLayout);
-            }
+        if(roles.contains("ROLE_MODERATOR")) {
+            addModeratorLabel(infoLayout);
         }
+        if(roles.contains("ROLE_STUDENT")) {
+            addStudentLabel(infoLayout, profileInfo);
+        }
+        if(roles.contains("ROLE_TEACHER")) {
+            addTeacherLabel(infoLayout, profileInfo);
+        }
+
         add(infoLayout);
+    }
+
+    private void addStudentLabel(HorizontalLayout infoLayout, UserProfile profileInfo) {
+        if(profileInfo.getStudentInfo() != null) {
+            VerticalLayout studentHelloLayout = new VerticalLayout();
+            Label label1 = new Label(String.format("Студент %s курса",
+                    profileInfo.getStudentInfo().getCourse()));
+            label1.getStyle().set("width", "max-content");
+            Label label2 = new Label(String.format("Группа %s", profileInfo.getStudentInfo().getGroupCode()));
+            label2.getStyle().set("width", "max-content");
+            studentHelloLayout.add(label1, label2);
+            studentHelloLayout.getStyle()
+                    .set("background-color", "lavenderblush")
+                    .set("border-radius", "var(--lumo-border-radius-s)")
+                    .set("width", "max-content");
+            studentHelloLayout.setSpacing(false);
+
+            infoLayout.add(studentHelloLayout);
+        }
+    }
+
+    private void addTeacherLabel(HorizontalLayout infoLayout, UserProfile profileInfo) {
+        if(profileInfo.getTeacherInfo() != null) {
+
+            VerticalLayout teacherHelloLayout = new VerticalLayout();
+            profileInfo.getTeacherInfo().getPositions().forEach(pos -> {
+                Label posLabel = new Label(pos.toString());
+                teacherHelloLayout.add(posLabel);
+            });
+            teacherHelloLayout.getStyle()
+                    //lavenderblush
+                    .set("background-color", "lavender")
+                    .set("border-radius", "var(--lumo-border-radius-s)");
+
+            infoLayout.add(teacherHelloLayout);
+        }
+    }
+
+    private void addModeratorLabel(HorizontalLayout infoLayout) {
+        VerticalLayout moderatorHelloLayout = new VerticalLayout();
+        Label label = new Label("Модератор");
+        moderatorHelloLayout.add(label);
+        moderatorHelloLayout.getStyle()
+                //lavenderblush
+                .set("background-color", "lavender")
+                .set("border-radius", "var(--lumo-border-radius-s)");
+
+        infoLayout.add(moderatorHelloLayout);
     }
 
     private void addCurrentScheduleLayout(UserProfile profileInfo) {
